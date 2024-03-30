51b510ff1e9ae9fa97e0e2caed5076e712606e1e
Shukhfayz <shukhfayz@gmail.com>
2024-03-30 04:42:59 -0400

    feat: publish common header component

--- src/app/components/headers/CommonNavbar.tsx
+++ src/app/components/headers/CommonNavbar.tsx
@@ -1,3 +1,56 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function CommonNavbar() {
-    return <div>Common Header</div>
+    const authMember = null;
+    return ( <div className="common-navbar">
+    <Container className="navbar-container">
+      <Stack className="menu">
+        <Box>
+          <NavLink to={"/"}>
+            <img className="brand-logo"
+              src="/icons/Burak.svg"
+              alt="Logo"
+            />
+          </NavLink>
+        </Box>
+        <Stack className="links">
+          <Box className={"hover-line"}>
+            <NavLink to={"/"}>Home</NavLink>
+          </Box>
+          <Box className={"hover-line"}>
+            <NavLink to={"/products"} activeClassName={"underline"}>Products</NavLink>
+          </Box>
+          {authMember &&
+            [
+              { path: "/orders", text: "Orders" },
+              { path: "/member-page", text: "My Page" },
+            ].map((link, index) => (
+              <Box key={index} className={"hover-line"}>
+                <NavLink to={link.path} activeClassName={"underline"}>{link.text}</NavLink>
+              </Box>
+            ))}
+            {/* BASKET */}
+          
+            <Box className={"hover-line"}>
+            <NavLink to={"/help"} activeClassName={"underline"}>Help</NavLink>
+          </Box>
+
+          {!authMember ? (
+            <Box>
+              <NavLink to={"/login"}><Button 
+              variant="contained"
+              className="login-button">Login</Button></NavLink>
+            </Box>
+          ) : (
+            <img className="user-avatar"
+            src="/icons/default-user.svg"
+            aria-haspopup="true"
+            />
+          )}
+        </Stack>
+      </Stack>
+    </Container>
+  </div>
+);
 }
