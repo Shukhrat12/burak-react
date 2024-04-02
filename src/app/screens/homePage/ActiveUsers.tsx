0f006e312a0a62f034490c1c456b1596d1e3f04a
Shukhfayz <shukhfayz@gmail.com>
2024-04-02 01:31:56 -0400

    feat: develop active users sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,12 +1,58 @@
-import { Container, Stack } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import { CardCover } from "@mui/joy";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Divider from "../../components/divider"
 
-function AtiveUsers() {
-    return <div className={"popularDishes"}>
-        <Container>
-        AtiveUsers
-            <Stack></Stack>
-        </Container>
+const userList = [
+  { productName: "Martin", imagePath: "/img/martin.webp" },
+  { productName: "Justin", imagePath: "/img/justin.webp" },
+  { productName: "Rose", imagePath: "/img/rose.webp" },
+  { productName: "Nusret", imagePath: "/img/nusret.webp" },
+];
+
+function ActiveUsers() {
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {userList.length !== 0 ? (
+                userList.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                            <img src={ele.imagePath} alt="image" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow variant="soft" className="product-detail">
+                        <Stack className="info">
+                            <Typography className={"name"}>
+                                {ele.productName}
+                            </Typography>
+                        </Stack>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">New Products are not available!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
+  );
 }
 
-export default AtiveUsers;
+export default ActiveUsers;
