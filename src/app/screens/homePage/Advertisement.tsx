e77d72b4368ad65a36826650483dbd8f2433b19d
Shukhfayz <shukhfayz@gmail.com>
2024-04-02 01:05:27 -0400

    feat: develop advertisement sectional component

--- src/app/screens/homePage/Advertisement.tsx
+++ src/app/screens/homePage/Advertisement.tsx
@@ -1,8 +1,15 @@
-import { Container, Stack } from "@mui/material";
-
 function Advertisement() {
-    return <div className={"popularDishes"}>
-        Advertisement
+    return <div className={"ads-restaurant-frame"}>
+        <video 
+        className={"ads-video"}
+        autoPlay={true}
+        loop
+        muted
+        playsInline
+        data-video-media=""
+        >
+            <source type="video/mp4" src="video/burak-ads.mp4" />
+        </video>
     </div>
 }
 
